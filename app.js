21427b872b493ac2676a49a1382dcc1f39f29608
ththongthai <soysay78@gmail.com>
2026-07-18 03:50:09 +0700

    connect firebase sdk

---
+++ app.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+//เปรียบเหมือนบัตรประชาชนของ App
+const firebaseConfig = {
+  apiKey: "AIzaSyCxgm57abKPktxBObHit1CUH80gjNtDY4s",
+  authDomain: "doctor2go-725a4.firebaseapp.com",
+  projectId: "doctor2go-725a4",
+  storageBucket: "doctor2go-725a4.firebasestorage.app",
+  messagingSenderId: "1066125809000",
+  appId: "1:1066125809000:web:88c609ac2e1f5e9094b2dd",
+};
+
+//เปิดใช้ Firebase โดยใช้ firebaseConfig  พอได้เก็บไว้ใน ----> firebaseApp
+const firebaseApp = initializeApp(firebaseConfig);
+//ขอระบบ Login ของ firebaseAppนี้ พอได้เอามาเก็บไว้ที่ตัวแปร ----> auth
+const auth = getAuth(firebaseApp);
+//แปลว่า ขอ Database ของ firebaseApp นี้ ค่าที่ได้เก็บไว้ใน ----> db
+const db = getFirestore(firebaseApp);
+
+//เอาไว้เก็บรหัสประจำตัวของ Liff App
+const LIFF_ID = "2010746451-zVL3e0wH";
+
+async function main() {
+  await liff.init({ liffId: LIFF_ID });
+
+  if (!liff.isLoggedIn()) {
+    liff.login();
+    return;
+  }
+
+  const profile = await liff.getProfile();
+  document.getElementById("status").textContent =
+    `สวัสดี ${profile.displayName}`;
+}
+
+main();
